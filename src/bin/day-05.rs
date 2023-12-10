92af70dfdceff5f1ec1ab7202d75072661381b63
Anshul Raman <anshul.raman@live.com>
2023-12-10 13:58:05 +0530

    fixed day5 part2

--- src/bin/day-05.rs
+++ src/bin/day-05.rs
@@ -49,14 +49,10 @@ mod part1 {
 }
 
 mod part2 {
-    // use super::*;
-
-    use std::collections::HashMap;
 
     pub fn process(input: &str) -> String {
         let mut seeds: Vec<i64> = vec![];
         let mut lookup: Vec<Vec<(i64, i64, i64)>> = vec![];
-        let mut ans = i64::MAX;
         for s in input.split('\n') {
             if s.starts_with("seeds") {
                 let (_, seeds_str) = s.split_once(':').unwrap();
@@ -76,59 +72,28 @@ mod part2 {
                 lookup[n].push((range_map[0], range_map[1], range_map[2]));
             }
         }
-
-        for l in lookup.iter_mut() {
-            l.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap())
-        }
-
-        for s_range in seeds.chunks_exact(2) {
-            for s in s_range[0]..s_range[0] + s_range[1] {
-                let mut seed_loc = s;
-                // dbg!(seed_loc);
-                for  l in lookup.iter() {
-                    // check memo
-                    // compute if not found in memo
-                    let mut mapped_val = seed_loc;
-                    // dbg!("mapping from:", idx, &l);
-
-                    // for m in l {
-                    //     if m.1 <= seed_loc && seed_loc <= m.1 + m.2 {
-                    //         mapped_val = seed_loc - m.1 + m.0;
-                    //         break;
-                    //     }
-                    // }
-                    //
-                    //
-
-                    // using binary search simmilar to loop above
-
-                    let mut lo: i64 = 0;
-                    let mut hi: i64 = l.len() as i64 - 1;
-                    while lo <= hi {
-                        let mid = (lo + hi) / 2;
-                        let rng = l[mid as usize];
-                        // dbg!("checking at", mid, seed_loc, rng);
-
-                        if rng.1 <= seed_loc && seed_loc <= rng.1 + rng.2 - 1 {
-                            mapped_val = seed_loc - rng.1 + rng.0;
-                            break;
-                        }
-
-                        if seed_loc < rng.1 {
-                            hi = mid - 1;
-                        } else {
-                            lo = mid + 1;
-                        }
-                        // dbg!(found, mapped_val, seed_loc, idx);
+        lookup.reverse();
+        let mut loc = 1;
+        loop {
+            let mut ans = loc;
+            for  l in &lookup {
+                for m in l {
+                    if m.0 <= ans && ans <= m.0 + m.2 {
+                        ans = ans - m.0 + m.1;
+                        break;
                     }
-                    seed_loc = mapped_val;
                 }
-
-                ans = ans.min(seed_loc);
             }
+
+            for s in seeds.chunks_exact(2) {
+                if s[0] <= ans && ans <= s[0] +  s[1] {
+                    return loc.to_string(); 
+                }
+            }
+
+            loc += 1;
         }
 
-        ans.to_string()
     }
 }
 
