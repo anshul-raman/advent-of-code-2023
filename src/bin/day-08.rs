f3cfb97e9411ad0e98547f1dd303c4e586603acc
Anshul Raman <anshul.raman@live.com>
2023-12-12 22:57:24 +0530

    day8 part2

--- src/bin/day-08.rs
+++ src/bin/day-08.rs
@@ -1,7 +1,7 @@
 fn main() {
     let input = include_str!("../inputs/day8.txt");
     dbg!(part1::process(input));
-    // dbg!(part2::process(input));
+    dbg!(part2::process(input));
 }
 
 mod part1 {
@@ -29,19 +29,20 @@ mod part1 {
             );
         });
         let mut idx = 0;
-        let mut ans = 1;
+        let mut ans = 0;
         let mut cur = "AAA";
         loop {
             let edges = graph.get(cur).unwrap();
+            if cur.eq("ZZZ") {
+                return ans.to_string();
+            }
 
             match steps[idx] {
                 'R' => cur = &edges.1,
                 'L' => cur = &edges.0,
                 _ => {}
             }
-            if cur.eq("ZZZ") {
-                return ans.to_string();
-            }
+
             idx = (idx + 1) % steps.len();
             ans += 1;
         }
@@ -49,10 +50,67 @@ mod part1 {
 }
 
 mod part2 {
-    
+    use std::collections::HashMap;
 
-    // pub fn process(input: &str) -> String {
-    // }
+    pub fn process(input: &str) -> String {
+        let (steps, graph_str) = input.trim().split_once("\n\n").unwrap();
+        let steps: Vec<char> = steps.chars().collect();
+
+        let mut graph = HashMap::<String, (String, String)>::new();
+
+        graph_str.split('\n').for_each(|f| {
+            let (node, edges) = f.split_once('=').unwrap();
+            let (a, b) = edges
+                .trim()
+                .trim_start_matches('(')
+                .trim_end_matches(')')
+                .split_once(',')
+                .unwrap();
+
+            graph.insert(
+                node.trim().to_string(),
+                (a.to_string(), b.trim().to_string()),
+            );
+        });
+
+        let starting_locs: Vec<&String> = graph.keys().filter(|k| k.ends_with('A')).collect();
+        let steps_req: Vec<u64> = starting_locs
+            .iter()
+            .map(|s| solve(&s, &steps, &graph))
+            .collect();
+
+        steps_req
+            .iter()
+            .fold(1u64, |acc, &f| (acc * f) / gcd(acc, f))
+            .to_string()
+    }
+
+    fn gcd(a: u64, b: u64) -> u64 {
+        if a == 0 {
+            return b;
+        }
+        return gcd(b % a, a);
+    }
+
+    fn solve(start: &str, steps: &Vec<char>, graph: &HashMap<String, (String, String)>) -> u64 {
+        let mut idx = 0;
+        let mut ans = 0;
+        let mut cur = start;
+        loop {
+            let edges = graph.get(cur).unwrap();
+            if cur.ends_with('Z') {
+                return ans;
+            }
+            match steps[idx] {
+                'R' => cur = &edges.1,
+                'L' => cur = &edges.0,
+                _ => {}
+            }
+
+            idx = (idx + 1) % steps.len();
+            ans += 1;
+        }
+    }
 }
 
 #[cfg(test)]
@@ -83,11 +141,20 @@ ZZZ = (ZZZ, ZZZ)";
         assert_eq!(part1::process(input), "6");
     }
 
-    // #[test]
-    // fn part2_test() {
-    //     let input = "";
-    //
-    //     let result = part2::process(input);
-    //     assert_eq!(result, "30")
-    // }
+    #[test]
+    fn part2_test() {
+        let input = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+
+        let result = part2::process(input);
+        assert_eq!(result, "6")
+    }
 }
